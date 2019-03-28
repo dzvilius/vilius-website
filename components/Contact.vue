4a0d4c7bfd699b72627d664c64cfaf1b13a46afa
Vilius Dzemyda <dev@viliusdzemyda.com>
2019-03-28 12:30:40 +0000

    create the Contact section component

---
+++ components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section ContactSection" id="Contact">
+    <div class="container">
+      <div class="ContactCta">
+        <h2 class="is-size-3">Are you hiring?</h2>
+        <p class="ContactCta__Subtitle">I am here to help you.</p>
+        <a href="#" class="button is-primary">Say Hi</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import '~assets/scss/_variables.scss';
+
+$contact-section-background: $color-sky-lighter !default;
+
+.ContactSection {
+  background: $contact-section-background;
+}
+
+.ContactCta {
+  text-align: center;
+  font-size: 18px;
+}
+
+.ContactCta__Subtitle {
+  margin: 12px 0 24px 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Header/>
     <About/>
+    <Contact/>
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
 import About from '~/components/About.vue'
+import Contact from '~/components/Contact.vue'
 
 export default {
   components: {
     Header,
     About,
+    Contact,
   },
 }
 </script>
