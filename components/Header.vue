d1ca95e9677c1369ff3e413c55ad1d6673c7abf9
Vilius Dzemyda <dev@viliusdzemyda.com>
2019-03-28 10:45:58 +0000

    create Header component

---
+++ components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <section class="hero">
+      <div class="hero__content">
+        <p class="hero-title">Hello :)</p>
+        <p class="hero-subtitle">
+          My name is Vilius,
+          <br>I am Front-End Web Developer,
+          <br>and I build web applications.
+        </p>
+        <a href="#" class="button is-primary">Say Hi</a>
+      </div>
+    </section>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import '~assets/scss/_variables.scss';
+
+$header-background-colour: $color-sky-light !default;
+$header-content-text-color: $color-ink !default;
+
+.header {
+  background-color: $header-background-colour;
+  position: relative;
+  height: 400px;
+
+  @media screen and (min-width: 1088px) {
+    height: 600px;
+  }
+}
+
+.hero {
+  position: absolute;
+  top: 50px;
+  left: 0;
+  right: 0;
+
+  @media screen and (min-width: 1088px) {
+    top: 150px;
+  }
+}
+
+.hero__content {
+  padding: 24px 0;
+  max-width: 600px;
+  margin: 0 auto;
+  color: $header-content-text-color;
+  text-align: center;
+}
+
+.hero-title {
+  font-size: 34px;
+  margin-bottom: 24px;
+}
+
+.hero-subtitle {
+  font-size: 20px;
+  margin-bottom: 24px;
+}
+</style>
