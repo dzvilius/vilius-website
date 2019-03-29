b586af8f7f58a44ce9ba657c2f11a08f0181c35a
Vilius Dzemyda <dev@viliusdzemyda.com>
2019-03-29 10:03:58 +0000

    change logo style

--- components/Logo.vue
+++ components/Logo.vue
@@ -7,9 +7,15 @@
 
 .Logo {
   font-family: 'Monoton', sans-serif;
+  background: rgba(255, 255, 255, 0.3);
   color: #223344;
-  padding: 12px;
-  font-size: 48px;
+  padding: 3px 6px 0 6px;
+  border-radius: 4px;
   line-height: 1;
+  font-size: 48px;
+
+  @media screen and (min-width: 1088px) {
+    font-size: 54px;
+  }
 }
 </style>
