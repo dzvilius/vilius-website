e1f07411057376e7159f2b1e85be50cae56b8ff3
Vilius Dzemyda <dev@viliusdzemyda.com>
2019-04-04 02:03:26 +0100

    rename a component class

--- components/Buttons/ButtonMail.vue
+++ components/Buttons/ButtonMail.vue
@@ -1,11 +1,11 @@
 <template>
-  <a href="mailto:hello@viliusdzemyda.com?subject=Hi" class="button is-primary MailButton">Say Hi</a>
+  <a href="mailto:hello@viliusdzemyda.com?subject=Hi" class="button is-primary ButtonMail">Say Hi</a>
 </template>
 
 <style lang="scss" scoped>
 @import '~assets/scss/_variables.scss';
 
-.MailButton {
+.ButtonMail {
   box-shadow: 2px 3px 0px #555577;
   font-weight: 500;
   text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.3);
